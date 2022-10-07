e0b469c159cbbea0577dabb0458ba8f2fa2efff2
ARPIT RIBADIYA <ar304626@dal.ca>
2022-10-07 18:50:56 -0300

    Added Success Test Case for Add Method

--- src/test/java/com/CanadaEats/group13/HelperTests.java
+++ src/test/java/com/CanadaEats/group13/HelperTests.java
@@ -6,7 +6,7 @@ public class HelperTests {
     @Test
     public void addTest() {
         Helper helper = new Helper();
-        int result = helper.add(5, 5);
-        Assertions.assertEquals(10, result);
+        int result = helper.add(10, 10);
+        Assertions.assertEquals(20, result);
     }
 }
