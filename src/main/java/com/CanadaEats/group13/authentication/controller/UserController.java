eb329251740dac33e87874ee6ee8c8ce9a54151c
Arpit Ribadiya <ar304626@dal.ca>
2022-12-08 11:21:14 -0400

    Added Authorization for All API Access of Project

--- src/main/java/com/CanadaEats/group13/authentication/controller/UserController.java
+++ src/main/java/com/CanadaEats/group13/authentication/controller/UserController.java
@@ -129,5 +129,4 @@ public class UserController {
         }
         return "redirect:/userloginpage";
     }
-
 }
--- src/main/java/com/CanadaEats/group13/filter/controller/FilterController.java
+++ src/main/java/com/CanadaEats/group13/filter/controller/FilterController.java
@@ -5,21 +5,14 @@ import com.CanadaEats.group13.filter.business.FilterBusiness;
 import com.CanadaEats.group13.filter.business.IFilterBusiness;
 import com.CanadaEats.group13.filter.dto.FilterDto;
 import com.CanadaEats.group13.filter.repository.FilterRepository;
-import com.CanadaEats.group13.utils.ApplicationConstants;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.CanadaEats.group13.utils.APIAccessAuthorization;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 public class FilterController {
@@ -32,15 +25,23 @@ public class FilterController {
 
     @GetMapping("/filters")
     public String getAllFilters(Model model, HttpServletRequest request) {
-        List<FilterDto> filters = filterBusiness.getAllFilters();
-        model.addAttribute("filters", filters);
-        return "/filter/displayFilters";
+        boolean isAPIAccessible = APIAccessAuthorization.getInstance().getAPIAccess(request);
+        if(isAPIAccessible)
+        {
+            List<FilterDto> filters = filterBusiness.getAllFilters();
+            model.addAttribute("filters", filters);
+            return "/filter/displayFilters";
+        }
+        return "redirect:/userloginpage";
     }
 
     @PostMapping("/updatefilters")
-    public String updateFilters(@ModelAttribute List<FilterDto> filterDto,Model model){
-        System.out.println("Request has reached");
-        filterBusiness.updateFilters(filterDto);
-        return "redirect:/filters";
+    public String updateFilters(@ModelAttribute List<FilterDto> filterDto,Model model, HttpServletRequest request){
+        boolean isAPIAccessible = APIAccessAuthorization.getInstance().getAPIAccess(request);
+        if(isAPIAccessible) {
+            filterBusiness.updateFilters(filterDto);
+            return "redirect:/filters";
+        }
+        return "redirect:/userloginpage";
     }
 }
