252f702a092886729741d8d7994153abb8882446
Arpit Ribadiya <ar304626@dal.ca>
2022-12-11 14:46:14 -0400

    Added TestCases for PAsswordEncode and Decode and APIAccessAuthorization

---
+++ src/test/java/com/CanadaEats/group13/utils/PasswordEncoderDecoderTest.java
@@ -0,0 +1,54 @@
+package com.CanadaEats.group13.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordEncoderDecoderTest {
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    final void encryptSuccessTest(){
+        String encryptString = "arpit1234";
+        String actualDecrytedString = "ftMn4rFs2L4HCX3lKnir8Q==";
+
+        String result = PasswordEncoderDecoder.getInstance().encrypt(encryptString);
+
+        assertEquals(actualDecrytedString, result);
+    }
+
+    @Test
+    final void encryptFailureTest(){
+        String encryptString = "arpit1234";
+        String wrongDecrytedString = "ftMn4rFs2L4HCX";
+
+        String result = PasswordEncoderDecoder.getInstance().encrypt(encryptString);
+
+        assertNotEquals(result, wrongDecrytedString);
+    }
+
+    @Test
+    final void decryptSuccessTest(){
+        String edecryptString = "ftMn4rFs2L4HCX3lKnir8Q==";
+        String actualEecrytedString = "arpit1234";
+
+        String result = PasswordEncoderDecoder.getInstance().decrypt(edecryptString);
+
+        assertEquals(actualEecrytedString, result);
+    }
+
+    @Test
+    final void decryptFailureTest(){
+        String edecryptString = "ftMn4rFs2L4HCX3lKnir8Q==";
+        String wrongEecrytedString = "arpit";
+
+        String result = PasswordEncoderDecoder.getInstance().decrypt(edecryptString);
+
+        assertNotEquals(wrongEecrytedString, result);
+    }
+}
