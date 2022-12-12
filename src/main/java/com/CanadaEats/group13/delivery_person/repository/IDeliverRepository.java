58307f815689761f26d1db78c510001484cc3689
Diwen Yao <dw395924@dal.ca>
2022-12-12 18:44:29 -0400

    delivery person functionality added

--- src/main/java/com/CanadaEats/group13/delivery_person/repository/IDeliverRepository.java
+++ src/main/java/com/CanadaEats/group13/delivery_person/repository/IDeliverRepository.java
@@ -1,8 +1,13 @@
 package com.CanadaEats.group13.delivery_person.repository;
 
+import com.CanadaEats.group13.order.dto.OrderDisplayDTO;
+
+import java.util.ArrayList;
+
 public interface IDeliverRepository {
-    public String getFirst_name();
-    public String getLast_name();
-    public void orders_assigned();
-    public void changestatus();
+    public ArrayList<OrderDisplayDTO> orders_assigned(String deliverId);
+    public boolean changeToDelivered(String orderId);
+    public boolean changeToPickedUp(String orderId);
+
+    public String orderTableChange(String changes, String checker);
 }
