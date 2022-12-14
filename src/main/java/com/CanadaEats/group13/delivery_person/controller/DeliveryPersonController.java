4e4bfd73e4b47aa95657e770781d7c015c039d0d
Manil Nirenkumar Patel <mn308169@dal.ca>
2022-12-14 15:33:47 -0400

    Delivery person business and TDD for restaurantOwner Admin

--- src/main/java/com/CanadaEats/group13/delivery_person/controller/DeliveryPersonController.java
+++ src/main/java/com/CanadaEats/group13/delivery_person/controller/DeliveryPersonController.java
@@ -1,56 +1,35 @@
 package com.CanadaEats.group13.delivery_person.controller;
 
 import com.CanadaEats.group13.database.DatabaseConnection;
+import com.CanadaEats.group13.delivery_person.business.DeliverPersonBusiness;
+import com.CanadaEats.group13.delivery_person.business.IDeliveryPersonBusiness;
 import com.CanadaEats.group13.delivery_person.repository.DeliverRepository;
-import com.CanadaEats.group13.delivery_person.repository.IDeliverRepository;
 import com.CanadaEats.group13.order.dto.OrderDTO;
-import com.CanadaEats.group13.order.repository.IOrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.sql.SQLException;
-import java.util.List;
 
 @Controller
 public class DeliveryPersonController {
-    IDeliverRepository deliverRepository;
+    IDeliveryPersonBusiness deliveryPersonBusiness;
 
     public DeliveryPersonController() {
-        this.deliverRepository = new DeliverRepository(DatabaseConnection.getInstance());
+        this.deliveryPersonBusiness = new DeliverPersonBusiness(new DeliverRepository(DatabaseConnection.getInstance()));
     }
 
     @GetMapping("/orders/{orderId}")
-    public String userRegistrationForm(Model model, @PathVariable("orderId") int orderId) {
-
-        List<OrderDTO> orders = new IOrderRepository().getOrders();
-        OrderDTO orderDisplayDTO = null;
-
-
-        for (OrderDTO displayDTO : orders) {
-            System.out.println("3" + displayDTO.getId());
-            if (displayDTO.getId() == orderId) {
-                orderDisplayDTO = displayDTO;
-            }
-        }
-
-        model.addAttribute("order", orderDisplayDTO
-        );
+    public String showOrders(Model model, @PathVariable("orderId") int orderId) {
+        OrderDTO orderDisplayDTO = deliveryPersonBusiness.displayOrders(orderId);
+        model.addAttribute("order", orderDisplayDTO);
         return "order/orderPage";
     }
 
     @GetMapping("/delivery/{orderId}/{orderStatus}/")
     public String updateOrderStatus(@PathVariable("orderId") String orderId, @PathVariable("orderStatus") int orderStatus) throws SQLException {
-        if (orderStatus == 2) {
-            System.out.println(orderId);
-            deliverRepository.changeToPickedUp(orderId);
-            System.out.println("pickedup");
-        } else {
-            deliverRepository.changeToDelivered(orderId);
-            System.out.println("delivered");
-        }
-
+        deliveryPersonBusiness.updateOrderStatus(orderId, orderStatus);
         return "redirect:/order";
     }
 }
